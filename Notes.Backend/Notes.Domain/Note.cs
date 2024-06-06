1895c5fa12950193bc61c087a257b438533a905f
smellilac <littleklyksa@gmail.com>
2024-06-06 16:25:14 +0300

    A clean architecture is laid down, Domain is filled

---
+++ Notes.Backend/Notes.Domain/Note.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notes.Domain;
+
+public class Note
+{
+    public Guid UserId { get; set; }
+    public Guid Id { get; set; }
+    public string Title { get; set; }
+    public string Details { get; set; }
+    public DateTime CreationDate { get; set; }
+    public DateTime? EditDate { get; set; }
+}
