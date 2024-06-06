f6974f0735b910a4a5aed3cdfe4c66968171c843
smellilac <littleklyksa@gmail.com>
2024-06-07 00:29:54 +0300

    Added AutoMapper configuration

---
+++ Notes.Backend/Notes.Application/Common/Mappings/IMapWith.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notes.Application.Common.Mappings;
+
+public interface IMapWith<T>
+{
+    public void Mapping(Profile profile) => profile.CreateMap(typeof(T), GetType()); 
+}
