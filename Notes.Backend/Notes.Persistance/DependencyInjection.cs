1be35ec501a75788dab307c28534fe6dc7115956
smellilac <littleklyksa@gmail.com>
2024-06-08 04:08:52 +0300

    Registered some services from application and persistance layers

---
+++ Notes.Backend/Notes.Persistance/DependencyInjection.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Notes.Application.Interfaces;
+using Microsoft.EntityFrameworkCore;
+namespace Notes.Persistance;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddPersistance(this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        var connectionString = configuration["DbConnection"];
+        services.AddDbContext<NotesDbContextPostgre>(options =>
+        {
+            options.UseNpgsql(connectionString);
+        });
+
+        services.AddScoped<INotesDbContext>(provider =>
+            provider.GetService<NotesDbContextPostgre>());
+
+        services.AddScoped<INotesDbContext>(provider =>
+        {
+            var dbContext = provider.GetService<NotesDbContextPostgre>();
+            if (dbContext == null)
+            {
+                throw new Exception("NotesDbContextPostgre service not found.");
+            }
+            return dbContext;
+        });
+        return services;
+    }
+}
